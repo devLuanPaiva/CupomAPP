c8347f3af2cb4c5013a4cd4b8b38f8beb8870bba
Luan Alves <devluanpaiva@gmail.com>
2025-05-14 16:47:17 -0300

    feat: create auth google system

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,5 +1,34 @@
-import { View } from "react-native";
+import { View, Button, Text } from "react-native";
+import {
+  GoogleSignin,
+  isSuccessResponse,
+  User,
+} from "@react-native-google-signin/google-signin";
+import { useState } from "react";
 
+GoogleSignin.configure({ webClientId: "" });
 export default function HomeScreen() {
-  return <View></View>;
+  const [auth, setAuth] = useState<User | null>(null);
+  const handleGoogleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      if (isSuccessResponse(userInfo)) {
+        setAuth(userInfo.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <View>
+      <Button onPress={handleGoogleSignIn} title="Sign in with Google" />
+      {auth && (
+        <View>
+          <Text>{auth.user.name}</Text>
+          <Text>{auth.user.email}</Text>
+        </View>
+      )}
+    </View>
+  );
 }
