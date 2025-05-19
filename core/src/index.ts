ad8c1219acf71c1c4ccb805f87797b52db8277d8
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-05-19 20:44:57 -0300

    chore: create core package

---
+++ core/src/index.ts
@@ -0,0 +1,17 @@
+import { IUser } from "./models/user/IUser.interface";
+import {
+  ICoordinates,
+  IAddress,
+  IBusiness,
+} from "./models/establishment/IEstablishment.interface";
+import { ICoupon, ICategory, IReview } from "./models/coupon/ICoupon.interface";
+
+export type {
+  IUser,
+  ICoordinates,
+  IAddress,
+  IBusiness,
+  ICoupon,
+  ICategory,
+  IReview,
+};
